08ef35caeb300276b9482dcd8437d3da9007ad99
Liz Abinante <eabinante@gmail.com>
2013-06-12 19:44:52 -0500

    AJAX with clearing forms

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,29 @@
+// This needs some OO-ification
+
 $(document).ready(function() {
-  $('li').on('click', '.login', function(event) {
+  $('a.nav').on('click', function(event) {
+    var goTo = $(this).attr('href');
     event.preventDefault();
-      $.get('/login', function(response) {
-        $('#empty').empty();
-        $('#empty').append(response);
-      });
-  });
-  $('li').on('click', '.signup', function(event) {
-    event.preventDefault();
-      $.get('/signup', function(response) {
+      $.get(goTo, function(response) {
         $('#empty').empty();
         $('#empty').append(response);
       });
   });
 });
+
+// $(document).ready(function() {
+//   $('li').on('click', '.login', function(event) {
+//     event.preventDefault();
+//       $.get('/login', function(response) {
+//         $('#empty').empty();
+//         $('#empty').append(response);
+//       });
+//   });
+//   $('li').on('click', '.signup', function(event) {
+//     event.preventDefault();
+//       $.get('/signup', function(response) {
+//         $('#empty').empty();
+//         $('#empty').append(response);
+//       });
+//   });
+// });
